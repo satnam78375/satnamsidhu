30df005b220a2d96e95e2c04b0aeeec3636fb1a9
satnamsidhu425@gmail.com <satnamsidhu425@gmail.com>
2018-06-05 22:23:36 -0700

    updated comment
    Signed-off-by: satnamsidhu425@gmail.com <satnamsidhu425@gmail.com>

--- Vehicle.java
+++ Vehicle.java
@@ -1,6 +1,6 @@
 /*
  * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templateshdhdhhdhdhbskabcsc
+ * To change this template file, choose Tools | Templateshdhdhhdhdhbskabc
  * and open the template in the editor.
  */
 
